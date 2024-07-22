970082b5059e88191cb335088eb4dc7ce770110a
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-21 20:24:55 -0500

    chore: initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center h-24 text-white max-w-[1240px] mx-auto px-4 '>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+            <ul className='flex hidden'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+            {/*icon from  reactt icons*/}
+            <div>
+                <AiOutlineMenu size={20}/>
+            </div>
+            <div className='fixed left-0 top-0 w-[60%]'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
